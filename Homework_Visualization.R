399e9d82f2355ff04ca6fc02e2e41c7680634ba5
INSERT YOUR NAME HERECM <cmgienger@gmail.com>
2019-11-01 11:29:35 -0500

    updated homework

---
+++ Homework_Visualization.R
@@ -0,0 +1,32 @@
+#######################################################
+######### Homework: Visualizations Using ggplot  ######
+#######################################################
+
+#Your homework script and outputs should be contained in a new R Project Folder (contains .Rproj file).
+
+#1. Using the mtcars dataset, make a scatterplot showing the relationship between displacement (predictor)
+#and horsepower (response).
+
+#2. Color the points by the number of cylinders.
+
+#3. Change the axes labels to Displacement (inches^3) and Horsepower. 
+
+#4. Annotate the plot with your name (so I know who made it)
+
+#5. Save the plot to the local directory.
+
+
+#6. Make a boxplot showing the distribution of miles per gallon as a function of the number of cylinders.
+
+#7. Add the raw data points on top of the boxplots.
+
+#8. Change axes lables to NUmber of Cylinders and Fuel Economy (miles per gallon).
+
+#9. Change the colors of the boxplot to be something other than the default.
+
+#10. Annotate plot with your name and save the plot to the local directory.
+
+# Send me your R Project File; contains the .Rproj file, the script, and the figure outputs.
+#You may have to zip before sending, depending on which email program you use.
+
+giengerc@apsu.edu
