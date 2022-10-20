978d8f98e88dfd8a92c07cd4d1b6b48084ffdee9
C.M. Gienger <39389399+cmgienger@users.noreply.github.com>
2022-10-20 16:11:15 -0500

    updated

---
+++ 4-Visualization.R
@@ -0,0 +1,134 @@
+#Ch4 - Data Visualization
+
+library(dplyr)
+library(ggplot2)
+library(gridExtra)
+
+#read in datafile
+compensation <- read.csv('compensation.csv')
+
+#plot data
+ggplot(compensation, aes(x = Root, y = Fruit)) +
+  geom_point()
+
+#add theme
+ggplot(compensation, aes(x = Root, y = Fruit)) +
+  geom_point() +
+  theme_bw()
+
+#add multiple customizations
+ggplot(compensation, aes(x = Root, y = Fruit, shape = Grazing, color = Grazing)) +
+  geom_point(size = 5) +
+  xlab("Root Biomass") +
+  ylab("Fruit Production") +
+  theme_bw()
+
+#box and whisker plots
+ggplot(compensation, aes(x = Grazing, y = Fruit)) +
+  geom_boxplot() +
+  xlab("Grazing treatment") +
+  ylab("Fruit Production") +
+  theme_bw()
+
+ggplot(compensation, aes(x = Grazing, y = Fruit)) +
+  geom_boxplot() +
+  geom_point(size = 4, color = 'lightgrey', alpha = 0.5) +
+  xlab("Grazing treatment") +
+  ylab("Fruit Production") +
+  theme_bw()
+
+#ggplot2 pre-defined colors
+browseURL("http://sape.inf.usi.ch/quick-reference/ggplot2/colour")
+
+
+#Plotting distribution of a single variable
+ggplot(compensation, aes(x = Fruit)) +
+  geom_histogram()
+
+#Most obvious thing to adjust is bin width:
+ggplot(compensation, aes(x = Fruit)) +
+  geom_histogram(bins = 10)
+
+ggplot(compensation, aes(x = Fruit)) +
+  geom_histogram(binwidth = 15)
+
+#faceting
+ggplot(compensation, aes(x = Fruit)) +
+  geom_histogram(binwidth = 15) +
+  facet_wrap(~Grazing)
+
+#save the plot
+ggsave("ThatCoolHistogramOfFruit.png")
+
+#scales and themes in ggplot2
+library(gridExtra)
+
+# BASE scatterplot
+eg_scatter <-
+  ggplot(data = compensation, aes(x = Root, y = Fruit)) +
+  geom_point()
+
+# BASE box-and-whiskers plot
+eg_box <-
+  ggplot(data = compensation, aes(x = Grazing, y = Fruit)) +
+  geom_boxplot()
+
+grid.arrange(eg_scatter, eg_box, nrow = 1)
+
+#plot customizations
+eg_scatter
+
+#add more intuitive axis labels
+eg_scatter + 
+  xlab("Root Biomass") + ylab("Fruit Production")
+
+#re-scale the axes (x,y)
+eg_scatter + 
+  xlim(0, 20) + ylim(0, 140)
+
+#putting custom text inside figure ; annotate() function
+eg_scatter +
+  annotate("text", x = c(6,8), y = c(105, 25),
+           label = c("Text up here...","...and text down here"))
+
+#adjust range of axes, frequency and location of tick marks
+eg_scatter + 
+  scale_x_continuous(limits = c(4, 11), breaks = 4:11)
+
+#customizing the colors allocated to each group using scale_colour_manual() layering.
+ggplot(data = compensation, aes(x = Root, y = Fruit, colour = Grazing)) +
+  geom_point() +
+  scale_colour_manual(values = c(Grazed = "brown", 	Ungrazed = "green"))
+
+#Scale transformation; one or both of the X-Y axes
+eg_box +
+  scale_y_continuous(breaks = seq(from = 10, to = 150, by = 20), trans = "log10")
+
+#modify theme elements; powerful framework for adjusting all the non-aesthetic pieces of a graph
+eg_scatter +
+  theme(
+    panel.background = element_rect(fill = NA, color = "black"),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(color = "lightblue")
+  )
+
+#the theme elements you can adjust
+? theme #click on help tab to see possible theme elements
+
+#here is a figure of different theme elements
+browseURL("https://henrywang.nl/ggplot2-theme-elements-demonstration/")
+
+#Some theme() syntax for axis titles and ticks
+eg_box +
+  theme(
+    axis.title.x = element_text(colour = "cornflowerblue", size =rel(2)),
+    axis.text.x = element_text(angle = 45, size = 13, vjust = 	0.5))
+
+#We use the scale_x_discrete() or scale_y_discrete() function to 
+#customize the labelling of groups
+eg_box + scale_x_discrete(limits = c("Ungrazed", "Grazed"), labels = c("Control", "Grazed"))
+
+#legend position
+ggplot(compensation, aes(x = Root, y = Fruit, color = Grazing)) +
+  geom_point() +
+  theme(legend.position = "none")
---
+++ 4-assignment.R
@@ -0,0 +1,39 @@
+#######################################################
+######### Homework: Visualizations Using ggplot  ######
+#######################################################
+
+#Your homework script and outputs should be contained in a new R Project Folder (contains .Rproj file).
+#We want outputs such as figures to be contained in our 'ecosystem'. 
+
+
+#1. Using the mtcars dataset, make a scatterplot showing the relationship between displacement (predictor)
+#and horsepower (response).
+
+#2. Color the points by the number of cylinders.
+#hint: If you 'str(mtcars)' you will notice that cyl a continuous numeric variable (either 4, 6, or 8 cylinders).
+#use as.factor(cyl) to force cyl to be a categorical variable.
+
+#3. Change the axes labels to Displacement (inches^3) and Horsepower. 
+
+#4. Annotate the plot with your name (so I know who made it)
+
+#5. Save the plot to the local directory (scripted command).
+
+#6. Make a boxplot showing the distribution of miles per gallon as a function of the number of cylinders.
+#hint: you should change the cyl variable from continuous to categorical using as.factor(cyl)
+
+#7. Add the raw data points on top of the boxplots.
+
+#8. Change axes labels to Number of Cylinders and Fuel Economy (miles per gallon).
+
+#9. Change the colors of the boxplot to be something other than the default.
+
+#10. Annotate plot with your name and save the plot to the local directory (scripted command, not a menu button).
+
+#11. Combine your scatterplot from questions 1-5 above with your boxplot from questions 6-10 above into a single 
+#figure in which the scatterplot is above the boxplot in a single column.   
+
+# Submit your R Project File to the Assignments area on D2L.
+# Your submission should be a NEW folder containing the .Rproj file, the script, and the figure outputs. It should
+# be a NEW project folder that you have created, not embedded in my example project from Github. 
+# You will have to zip (compress) your project folder before uploading to D2L.
