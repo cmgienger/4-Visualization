d5156dc07bd38ef437f3562ccb3be0102fce3c90
C.M. Gienger <39389399+cmgienger@users.noreply.github.com>
2022-10-20 16:07:48 -0500

    updated

--- 4-assignment.R
+++ 4-assignment.R
@@ -10,6 +10,8 @@
 #and horsepower (response).
 
 #2. Color the points by the number of cylinders.
+#hint: If you 'str(mtcars)' you will notice that cyl a continuous numeric variable (either 4, 6, or 8 cylinders).
+#use as.factor(cyl) to force cyl to be a categorical variable.
 
 #3. Change the axes labels to Displacement (inches^3) and Horsepower. 
 
@@ -18,6 +20,7 @@
 #5. Save the plot to the local directory (scripted command).
 
 #6. Make a boxplot showing the distribution of miles per gallon as a function of the number of cylinders.
+#hint: you should change the cyl variable from continuous to categorical using as.factor(cyl)
 
 #7. Add the raw data points on top of the boxplots.
 
@@ -27,6 +30,10 @@
 
 #10. Annotate plot with your name and save the plot to the local directory (scripted command, not a menu button).
 
+#11. Combine your scatterplot from questions 1-5 above with your boxplot from questions 6-10 above into a single 
+#figure in which the scatterplot is above the boxplot in a single column.   
+
 # Submit your R Project File to the Assignments area on D2L.
-# Your submission should be a folder containing the .Rproj file, the script, and the figure outputs.
-# You may have to zip (compress) the folder before uploading.
+# Your submission should be a NEW folder containing the .Rproj file, the script, and the figure outputs. It should
+# be a NEW project folder that you have created, not embedded in my example project from Github. 
+# You will have to zip (compress) your project folder before uploading to D2L.
